70886f63524543bb1a39ccea3a9a3f5722703fb0
zi-jing <zi-jing@users.noreply.github.com>
2020-02-05 17:10:58 +0800

    重构前备份

--- src/main/java/com/zi_jing/lotterymachine/commands/CommandLotteryMachine.java
+++ src/main/java/com/zi_jing/lotterymachine/commands/CommandLotteryMachine.java
@@ -1,16 +1,24 @@
 package com.zi_jing.lotterymachine.commands;
 
+import com.google.common.collect.ImmutableMap;
 import com.mojang.brigadier.CommandDispatcher;
 import com.zi_jing.lotterymachine.LotteryMachine;
-import com.zi_jing.lotterymachine.entity.EntityBox;
 import com.zi_jing.lotterymachine.network.PacketLotteryMachine;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.EndPortalBlock;
+import net.minecraft.block.EndPortalFrameBlock;
+import net.minecraft.block.EnderChestBlock;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.world.World;
+import net.minecraft.state.IProperty;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraftforge.fml.network.PacketDistributor;
 
 public class CommandLotteryMachine {
@@ -25,20 +33,28 @@ public class CommandLotteryMachine {
 	}
 	
 	private static void run(ServerPlayerEntity player) {
+		BlockPos pos = ((BlockRayTraceResult)player.func_213324_a(20.0D, 0.0F, false)).getPos();
+		if(player.world.getBlockState(new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ())).getBlock() != Blocks.AIR) {LotteryMachine.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), new PacketLotteryMachine(2)); return;}
 		ItemStack currentStack = player.inventory.mainInventory.get(player.inventory.currentItem);
 		if(currentStack.getItem() != Items.GOLD_NUGGET || currentStack.getCount() <= 5) {
 			LotteryMachine.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), new PacketLotteryMachine(1));
 		} else {
 			if(currentStack.getCount() == 5) player.inventory.mainInventory.set(player.inventory.currentItem, ItemStack.EMPTY);
 			if(currentStack.getCount() > 5) player.inventory.mainInventory.set(player.inventory.currentItem, new ItemStack(currentStack.getItem(), currentStack.getCount() - 5));
+			//try {
+			//player.world.setBlockState(new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ()), new BlockState(Blocks.ENDER_CHEST , ImmutableMap.<IProperty<?>, Comparable<?>>builder().put(EnderChestBlock.FACING, Direction.byIndex(2)).put(EnderChestBlock.WATERLOGGED, false).build()).with(EnderChestBlock.WATERLOGGED, false).with(EnderChestBlock.FACING, (player.getHorizontalFacing().getIndex() <= 1) ? Direction.byIndex((int)Math.random() * 4 + 1) : player.getHorizontalFacing()));
+			//} catch(Exception e) {e.printStackTrace();}
+			player.world.setBlockState(new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ()), EndPortalBlock.getStateById(5134));
 			LotteryMachine.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), new PacketLotteryMachine(0));
+			
 		}
-		World world = player.world;
-		EntityBox entity = new EntityBox(EntityBox.TYPE, world);
-		entity.posX = player.posX;
-		entity.posY = player.posY;
-		entity.posZ = player.posZ;
-		world.addEntity(entity);
+		
+		//EntityBox entity = new EntityBox(EntityBox.TYPE, world);
+		//entity.posX = player.posX;
+		//entity.posY = player.posY;
+		//entity.posZ = player.posZ;
+		//world.addEntity(entity);
+		
 	}
 
 }
--- src/main/java/com/zi_jing/lotterymachine/network/PacketLotteryMachine.java
+++ src/main/java/com/zi_jing/lotterymachine/network/PacketLotteryMachine.java
@@ -4,15 +4,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.function.Supplier;
 
-import com.zi_jing.lotterymachine.LotteryMachine;
-
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.fml.network.NetworkDirection;
 import net.minecraftforge.fml.network.NetworkEvent;
@@ -20,18 +17,34 @@ import net.minecraftforge.fml.network.NetworkEvent;
 public class PacketLotteryMachine {
 	
 	public int id;
+	public ItemStack stack = ItemStack.EMPTY;
+	public BlockPos pos;
 	
 	// 0=服务器->客户端：开始抽奖，显示动画	1=服务器->客户端：金粒不足
-	// 2=客户端->服务器：动画结束，发放奖励
+	// 2=服务器->客户端：方块位置不可用		3=客户端->服务器：动画结束，发放奖励
 	public PacketLotteryMachine(int packetId) {
 		this.id = packetId;
 	}
 	
+	public PacketLotteryMachine(int packetId, ItemStack stack) {
+		this.id = packetId;
+		this.stack = stack;
+	}
+	
+	public PacketLotteryMachine(int packetId, BlockPos pos) {
+		this.id = packetId;
+		this.pos = pos;
+	}
+	
 	public static PacketLotteryMachine fromBytes(PacketBuffer buf) {
-		return new PacketLotteryMachine(buf.readInt());
+		int packetId = buf.readInt();
+		if(packetId == 3) return new PacketLotteryMachine(packetId, buf.readItemStack());
+		
+		return new PacketLotteryMachine(packetId);
 	}
 	
 	public static void toBytes(PacketLotteryMachine msg, PacketBuffer buf) {
+		if(msg.id == 3) {buf.writeInt(msg.id); buf.writeItemStack(msg.stack); return;}
 		buf.writeInt(msg.id);
 	}
 	
@@ -39,9 +52,7 @@ public class PacketLotteryMachine {
 		switch(msg.id) {
 		case 0:
 			if(ctx.get().getDirection() == NetworkDirection.PLAY_TO_CLIENT) {
-				
 				ctx.get().enqueueWork(new Runnable() {
-
 					@Override
 					public void run() {
 						Minecraft.getInstance().player.sendStatusMessage(new StringTextComponent("§e§l抽奖已开始，祝你好运！"), false);
@@ -65,24 +76,31 @@ public class PacketLotteryMachine {
 							public void run() {
 								time++;
 								
-								Minecraft.getInstance().world.addParticle(ParticleTypes.FLAME, x, y, z, 0.5, 2, 0);
-								Minecraft.getInstance().world.addParticle(ParticleTypes.FLAME, x, y, z, -0.5, 2, 0);
-								Minecraft.getInstance().world.addParticle(ParticleTypes.FLAME, x, y, z, 0, 2, 0.5);
-								Minecraft.getInstance().world.addParticle(ParticleTypes.FLAME, x, y, z, 0, 2, -0.5);
+								if(time <= 200) {
+									Minecraft.getInstance().world.addParticle(ParticleTypes.FLAME, x, y, z, 0.5, 2, 0);
+									Minecraft.getInstance().world.addParticle(ParticleTypes.FLAME, x, y, z, -0.5, 2, 0);
+									Minecraft.getInstance().world.addParticle(ParticleTypes.FLAME, x, y, z, 0, 2, 0.5);
+									Minecraft.getInstance().world.addParticle(ParticleTypes.FLAME, x, y, z, 0, 2, -0.5);
+								}
 								
-								if(time > 200)
+								if(time > 200) {
 									this.cancel();
+								}
 							}
 							
 						}, 10, 10);
+						
 					}
 					
 				});
 			};
+			break;
 		case 1:
-			if(ctx.get().getDirection() == NetworkDirection.PLAY_TO_CLIENT)
-				Minecraft.getInstance().player.sendStatusMessage(new StringTextComponent("§c你的金粒好像不够了qwq"), true);
-				
+			if(ctx.get().getDirection() == NetworkDirection.PLAY_TO_CLIENT) Minecraft.getInstance().player.sendStatusMessage(new StringTextComponent("§c你的金粒好像不够了qwq"), true);
+			break;
+		case 2:
+			if(ctx.get().getDirection() == NetworkDirection.PLAY_TO_CLIENT) Minecraft.getInstance().player.sendStatusMessage(new StringTextComponent("§c准心必须指向地面哦"), true);
+			break;
 		}
 		ctx.get().setPacketHandled(true);
 	}
